0dc03bbaefcc8b1730da34ef96a989cf9c4fded6
Diego-Chiola <chioladiego@gmail.com>
2023-11-09 14:59:33 +0100

    Added test: complexAddRealPart

--- src/test/java/com/example/ComplexTest.java
+++ src/test/java/com/example/ComplexTest.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,5 +20,13 @@ public class ComplexTest
         Complex calc = new Complex(0.0, 4.0);
         assertNotNull(calc);
         assertTrue(calc.isPure());
+    }
+
+    @Test
+    void complexAddRealPart(){
+        Complex c1 = new Complex(1, 1);
+        Complex c2 = new Complex(2, 2);
+        double expected = 3;
+        assertEquals(expected, c1.add(c2));
     }
 }
