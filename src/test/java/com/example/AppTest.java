e8bcf4a240f3bd5340589e5eb39128f61d742051
Giomit99 <81476436+Giomit99@users.noreply.github.com>
2023-11-16 18:08:38 +0100

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -5,10 +5,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test
-    void AppMainTest() {
-        App app= new App();
-        app.main();
-        assert(true);
-    }
 }
