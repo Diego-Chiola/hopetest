88e804d73ba272f6cedc8907cdbbb8b0bbcdab8e
Giomit99 <81476436+Giomit99@users.noreply.github.com>
2023-11-16 17:56:25 +0100

    Update GCDTest.java

--- src/test/java/com/example/GCDTest.java
+++ src/test/java/com/example/GCDTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GCDTest{
+ 
     @Test
     void GCDtest(){
     }
